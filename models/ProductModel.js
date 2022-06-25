e2d072e42b1f3a5e207954df1767b6c0651fd4bb
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-06-25 19:33:47 +0300

    UserSchema, ProductSchema created

---
+++ models/ProductModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true},
+        description: { type: String, required: true },
+        prdImg: { type: String, required: true },
+        categories: { type: Array },
+        size: { type: String },
+        voting: { type: Number },
+        price: { type: Number, required: true}
+    },
+    { timestamps: true}
+)
+
+module.exports = mongoose.model("ProductModel", ProductSchema)
---
+++ models/UserModel.js
@@ -0,0 +1,14 @@
+const mongoose  = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+    {
+        username : { type: String, required: true, unique: true},
+        email : { type: String, required: true, unique: true},
+        password: { type: String, required: true},
+        isAdmin: { type: Boolean, default: false},
+        userImg: { type: String, default: "default.png"}
+    },
+    { timestamps : true }
+)
+
+module.exports = mongoose.model("UserModel", UserSchema)
