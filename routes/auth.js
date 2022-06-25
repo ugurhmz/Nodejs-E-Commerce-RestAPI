1280ccbf6f86abed4e673f9d23c67b4a36344299
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-06-25 21:05:09 +0300

    Jsonwebtoken usage then Login success with CryptoJs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const UserModel =  require("../models/UserModel")
 const CryptoJs = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -19,5 +20,50 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err)
     } 
 })
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const hasUser = await UserModel.findOne({  username : req.body.username })
+        
+       if (!hasUser) {
+           return res.status(401).json("User not found, try again!")
+       }
+        
+        const hashedPassword = CryptoJs.AES.decrypt (
+            hasUser.password,
+            process.env.PAS_SECURITY
+        )
+
+        const dbPassword = hashedPassword.toString(CryptoJs.enc.Utf8)
+       if  (dbPassword !== req.body.password) {
+           return  res.status(401).json("Your password is wrong please fix it!")
+       }
+     const accessToken = jwt.sign(
+            {
+                id: hasUser._id,
+                isAdmin: hasUser.isAdmin
+            },
+            process.env.JWT_SECURITY,
+            { 
+                expiresIn: "7d"
+            }
+        );
+
+        const { password, ...exceptThePassword} = hasUser._doc
+
+        // response 
+        const loginMsg = "Login success."
+        res.status(200).json({
+            ...exceptThePassword,
+            accessToken,
+            loginMsg
+        })
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 
 module.exports = router;
