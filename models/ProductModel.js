6f5b391c53c749f697b09af7dd4a94c137ee5e0e
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-06-27 01:11:07 +0300

    Get all categories added and ProductSchema changed with multiple categories

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema(
         title: { type: String, required: true, unique: true},
         description: { type: String, required: true },
         prdImg: { type: String, required: true },
-        categories: { type: Array },
+        categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CategoryModel', required: true }],
         size: { type: String },
         voting: { type: Number },
         price: { type: Number, required: true}
--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,7 @@ const CategoryModel = require("../models/CategoryModel")
 const {  verifyTokenAdmin}  = require("../middleware/tokenVerify")
 const router = require("express").Router()
 
-
+// CREATE CATEGORY
 router.post("/create", verifyTokenAdmin, async (req, res) => {
     const { name } =   req.body
 
@@ -21,6 +21,31 @@ router.post("/create", verifyTokenAdmin, async (req, res) => {
         res.status(500)
     }
 
+})
+
+// GET ALL CATEGORIES
+router.get("/get-all", async (req,res) => {
+    try {
+        let allCategories = await CategoryModel.find()
+        res.status(200).json(allCategories)
+    } catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+// GET ONE CATEGORY
+router.get("/:id", async (req, res) => {
+
+    if (req.params.id < 0) { return res.status(500).json({error: 'Id number cannot be less than zero'})}
+    try {
+        const category = await CategoryModel.findById(req.params.id)
+        res.status(200).json(category)
+    } catch(err) {
+        res.status(500).json({
+            err,
+            msg:"Category Not Found"
+        })
+    }
 })
 
 module.exports =  router
