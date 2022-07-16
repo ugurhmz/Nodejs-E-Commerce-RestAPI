e58152a2ae7aaa3ad46553c670087157bc85f382
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-07-16 14:56:49 +0300

    User favourites items added and tested

--- controllers/Favourites.js
+++ controllers/Favourites.js
@@ -1,3 +1,4 @@
+const httpStatus = require("http-status");
 const FavouriteModel = require("../models/FavouriteModel");
 const ProductModel = require("../models/ProductModel");
 
@@ -37,5 +38,40 @@ exports.createFavouriteController = async (req, res) => {
     }
   } catch (error) {
     res.status(500).send("something went wrong");
+  }
+};
+
+// GET ALL USER FAVOURITES
+exports.getUserFavouritesController = async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const favs = await FavouriteModel.findOne({ owner: req.params.id }).populate([
+      {
+        path: "items.itemId",
+        model: "ProductModel",
+        populate: {
+          path: "categories",
+          model: "CategorModel",
+          select: "_id name  categoryImg createdAt",
+        },
+      },
+      {
+        path: "owner",
+        model: "UserModel",
+        select: "_id username email isAdmin userImg createdAt",
+      },
+    ]);
+
+    if (favs.items.length == 0) {
+      try {
+        await FavouriteModel.findByIdAndDelete(favs.id);
+      } catch (err) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
+      }
+    }
+
+    res.status(httpStatus.OK).json(favs);
+  } catch (err) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
   }
 };
--- routes/favourites.js
+++ routes/favourites.js
@@ -1,7 +1,8 @@
-const { tokenVerify } = require("../middleware/tokenVerify.js");
+const { tokenVerify, tokenVerifyAuthorization } = require("../middleware/tokenVerify.js");
 const router = require("express").Router();
-const { createFavouriteController } = require("../controllers/Favourites");
+const { createFavouriteController, getUserFavouritesController } = require("../controllers/Favourites");
 
 router.post("/", tokenVerify, createFavouriteController);
+router.get("/user-id/:id", tokenVerifyAuthorization, getUserFavouritesController);
 
 module.exports = router;
